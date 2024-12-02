61be1ea6b86b89523258a6c1b9ef7a1ff3a1f7ce
Margaux Imbert <margaux69.imbert@gmail.com>
2024-12-02 19:15:07 +0100

    kmertree class almost done

--- kmer_tree.py
+++ kmer_tree.py
@@ -5,14 +5,16 @@ class KMerTree ():
     def __init__(self, name):
         self.name = name
         self.children = {}
+        self.reads = set()
 
 
-    def add_kmer(self, kmer : str) -> None :
+    def add_kmer(self, kmer : str, read_name: str) -> None :
         if len(kmer) == 0 :
+            self.reads.add(read_name)
             return None
         if kmer[0] not in self.children :
             self.children[kmer[0]] = KMerTree(kmer[0])
-        self.children[kmer[0]].add_kmer(kmer[1:])
+        self.children[kmer[0]].add_kmer(kmer[1:], read_name)
 
     def has_child(self, car : str) -> bool :
         return car in self.children
@@ -21,3 +23,19 @@ class KMerTree ():
         if car not in self.children:
             raise KeyError("No child named ", car)
         return self.children[car]
+
+    def get_last_node(self, kmer : str) -> 'KMerTree' or None :
+        if len(kmer) == 0:
+            return self
+        if kmer[0] not in self.children :
+            return None
+        return self.children[kmer[0]].get_last_node(kmer[1:])
+
+    def remove_kmer(self, kmer, read_name) -> bool :
+        # pas finie
+        if not self.children :
+            self.reads.remove(read_name)
+            if not self.reads :
+                return True
+        if self.remove_kmer(kmer, read_name):
+            self.children.pop(kmer, read_name)
--- test_kmer_tree.py
+++ test_kmer_tree.py
@@ -1,12 +1,42 @@
 from kmer_tree import *
 import pytest
 
+def create_tree () :
+    tree = KMerTree("root")
+    tree.add_kmer("ACGT", "1s")
+    tree.add_kmer("GCTT", "1s")
+    tree.add_kmer("ACTA", "2s")
+    tree.add_kmer("ACGT", "2a")
+    tree.add_kmer("TTTT", "2a")
+    return tree
+
+
 def test_kmer_initialisation () :
     tree = KMerTree("root")
-    tree.add_kmer("ACGT")
+
+    tree.add_kmer("ACGT", "1a")
+    tree.add_kmer("ACGT", "2s")
     assert tree.has_child("A") is True
     assert tree.has_child("G") is False
     anode1 = tree.get_child("A")
     cnode2 = anode1.get_child("C")
     gnode3 = cnode2.get_child("G")
     tnode4 = gnode3.get_child("T")
+    assert tnode4.reads == {"1a", "2s"}
+
+    tree.add_kmer("ACTA", "1a")
+    tnode3 = cnode2.get_child("T")
+    anode4 = tnode3.get_child("A")
+    assert anode4.reads == {"1a"}
+
+
+def test_get_last_node () :
+    tree = create_tree()
+    anode1 = tree.get_child("A")
+    cnode2 = anode1.get_child("C")
+    gnode3 = cnode2.get_child("G")
+    tnode4 = gnode3.get_child("T")
+    assert tree.get_last_node("ACGT") == tnode4
+    assert tree.get_last_node("ACG") == gnode3
+
+
